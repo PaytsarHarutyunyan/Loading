3d5ffce313999f4c60e6b487f585b15f8d46b874
@paytsar.harutyunyan1994 <paytsar.harutyunyan1994@gmail.com>
2023-03-21 12:10:35 +0400

    Crated files

---
+++ index.js
@@ -0,0 +1,8 @@
+setTimeout(() => {
+  document.querySelector('.loading-container').style.display = 'flex';
+}, 500);
+
+
+setTimeout(() => {
+  document.querySelector('.loading-container').style.display = 'none';
+}, 7000);
